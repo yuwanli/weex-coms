22f8384e73d4f6a83b23bdc0d648d157c3c19c97
wanli.yu <wanli.yu@beibei.com>
2019-03-19 23:53:51 +0800

    初始化项目

---
+++ examples/popup/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="weex-demo">
+    <div class="btn" @click="showPopup = true">show</div>
+    <div class="btn" @click="showPopup = false">hide</div>
+    <weex-popup :show-popup="showPopup" :show-close=true @hidePopup="showPopup = false">
+      <div class="content" slot="content">
+        <text>11111</text>
+      </div>
+    </weex-popup>
+  </div>
+</template>
+<script>
+  import weexPopup from '../../components/weex-popup/index.js';
+  export default {
+    components: { weexPopup },
+    data: () => ({
+      showPopup: false
+    }),
+    methods: {
+    }
+  };
+</script>
+
+<style lang="less" scoped>
+.weex-demo{
+  flex: 1;
+}
+.content {
+  width: 540px;
+  border-radius: 5px;
+  padding: 24px 32px;
+  background-color: #fff;
+}
+</style>
---
+++ examples/test/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <weex-popup :show-popup="showPopup" :show-close="false" :default-close="false" @hidePopup="showPopup = false">
+            <text slot="content">111111</text>
+        </weex-popup>
+    </div>
+</template>
+<script>
+  import weexPopup from '../../components/weex-popup/index.js';
+  export default {
+    components: { weexPopup },
+    data: () => ({
+        showPopup: true,
+    }),
+    methods: {
+    }
+  };
+</script>
+<style>
+</style>
