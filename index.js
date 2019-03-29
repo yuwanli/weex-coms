7bf132a7614cf994200cfb2171b98b5263cd03a1
wanli.yu <wanli.yu@beibei.com>
2019-03-30 01:29:56 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/**
- * CopyRight (C) 2017-2022 Alibaba Group Holding Limited.
- * Created by Tw93 on 17/09/25
- */
-
 import WeexPopup from './components/weex-popup';
+import WeexLamp from './components/weex-lamp';
 
 export {
     WeexPopup,
+    WeexLamp
 };
